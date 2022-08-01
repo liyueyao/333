1d6444d961a635d9e97f972b17cd2cfd6e1c9a65
liyueyao <975295916@qq.com>
2022-08-01 23:02:39 +0800

    afsfs

---
+++ src/fdd.java
@@ -0,0 +1,8 @@
+public class fdd {
+    public static void main(String[] args){
+        System.out.println("hello world!! 1 ");
+        System.out.println("hello world!! 2 ");
+        System.out.println("hello world!! 3 ");
+        System.out.println("hello world!! 3 ");
+    }
+}
